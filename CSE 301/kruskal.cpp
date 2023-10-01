937a8b8aa0a403274f29bbdd725427092f34e2c8
Piccolo7210 <114597646+Piccolo7210@users.noreply.github.com>
2023-10-01 21:12:42 +0600

    Update kruskal.cpp

--- CSE 301/kruskal.cpp
+++ CSE 301/kruskal.cpp
@@ -7,8 +7,8 @@ int vertex;
 vector<set<string>> Vset; 
 vector<string> Vname;
 set<pair<int,pair<string,string>>> ecost;
-void kruskal();
-int Findset(vector<set<string>> &Vset,string name);
+void kruskalf();
+int FindSet(string name);
 void Makeset( );
 void read();
 int indexnum(string str);
@@ -17,7 +17,7 @@ set<pair<int,pair<string,string>>> A;
 int main(){
     read();
     Makeset();
-    kruskal();
+    kruskalf();
     print();
 }
 void Makeset(){
@@ -29,14 +29,10 @@ void Makeset(){
     }
 }
 void read(){
-        int i,j;
+        int i,j,m,n;
         for(i=0;i<Size;i++){
         for(j=0;j<Size;j++){
-            if(i==j)graph[i][j]=0;
-            else{
             graph[i][j]=INT_MAX;
-            
-            }
         }
     }
         freopen("kruskal.txt","r",stdin);
@@ -45,43 +41,38 @@ void read(){
             string x,y;
             int cost;
             pair <string,string>edg;
-
-        cin>>x>>y>>cost;
-        if(find(Vname.begin(),Vname.end(),x)==Vname.end())Vname.push_back(x);
-        if(find(Vname.begin(),Vname.end(),y)==Vname.end())Vname.push_back(y);
-        int m,n;
-        m=indexnum(x);
-        n=indexnum(y);
-        graph[m][n]=cost;
-        graph[n][m]=cost;
-        edg.first=x;
-        edg.second=y;
-        pair<int,pair<string,string>> edcost;
-        edcost.first=cost;
-        edcost.second=edg;
-        ecost.insert(edcost);
+            cin>>x>>y>>cost;
+            if(find(Vname.begin(),Vname.end(),x)==Vname.end())Vname.push_back(x);
+            if(find(Vname.begin(),Vname.end(),y)==Vname.end())Vname.push_back(y);
+            m=indexnum(x);
+            n=indexnum(y);
+            graph[m][n]=cost;
+            graph[n][m]=cost;
+            edg.first=x;
+            edg.second=y;
+            pair<int,pair<string,string>> edcost;
+            edcost.first=cost;
+            edcost.second=edg;
+            ecost.insert(edcost);
     }
        vertex=Vname.size(); 
     }
-int Findset(vector<set<string>> &Vset,string name){
-    for(int i=0;i<vertex;i++){
-        if(Vset[i].find(name) == Vset[i].end()){
-            return -1;
+int FindSet(string name){
+    for(int i=0; i<Vset.size() ;i++){
+        if(Vset[i].find(name) != Vset[i].end()){
+            return i;
         }
-        else return i;
     }
+    return -1;
 }
-void kruskal(){
+void kruskalf(){
     int i,u,v;
     for(auto it :ecost){
-        u=Findset(Vset,it.second.first);
-        v=Findset(Vset,it.second.second);
+        u=FindSet(it.second.first);
+        v=FindSet(it.second.second);
         if(u!=v){
            A.insert(it);
-           //auto it = Vset.begin();
-        //    it=find(Vset.begin(),Vset.end(),Vname[u]);
-        //    (*it).insert(Vname[v]); 
-        Vset[indexnum(it.second.first)].insert(it.second.second);
+        for(auto x : Vset[v])Vset[u].insert(x);
            auto itr = Vset.begin()+v;
            Vset.erase(itr);
         }
@@ -99,5 +90,5 @@ void print(){
         minDis+=it.first;
         cout<<it.second.first<<" --> "<<it.second.second<<" : "<<it.first<<endl;
     }
-    cout<<"Total DIstance : "<<minDis<<endl;
+    cout<<"Total Distance : "<<minDis<<endl;
 }
