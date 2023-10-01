3f1ebb7547ed130c9f98542f145cb03da076f945
Piccolo7210 <114597646+Piccolo7210@users.noreply.github.com>
2023-10-01 19:10:36 +0600

    Update kruskal.cpp

--- CSE 301/kruskal.cpp
+++ CSE 301/kruskal.cpp
@@ -2,73 +2,102 @@
 using namespace std;
 #define Size 100
 int graph[Size][Size];
+int edge;
+int vertex;
+vector<set<string>> Vset; 
 vector<string> Vname;
-void kruskal(set<pair<int,pair<string,string>>> &ecost,vector<set<string>> &Vset,int vertex,int edge);
-int Findset(vector<set<string>> &Vset,string name,int vertex);
-void Makeset(vector<set<string>> &Vset,int vertex );
-void Makeedge(set<pair<int,pair<string,string>>> &ecost,int edge);
+set<pair<int,pair<string,string>>> ecost;
+void kruskal();
+int Findset(vector<set<string>> &Vset,string name);
+void Makeset( );
+void read();
+int indexnum(string str);
+void print();
+set<pair<int,pair<string,string>>> A;
 int main(){
-    int vertex,edge;
-    vector<set<string>> Vset;              
-    cin>>vertex>>edge;
-    for(int i=0;i<vertex;i++)for(int j=0;j<vertex;j++)graph[i][j]=0;
-    for(int i=0;i<vertex;i++){
-        string s;
-        cin>>s;
-        Vname.push_back(s);
-    }
-    set<pair<int,pair<string,string>>> ecost;
-    
+    read();
+    Makeset();
+    kruskal();
+    print();
 }
-void Makeset(vector<set<string>> &Vset,int vertex ){
+void Makeset(){
          int i;
     for(i=0;i<vertex;i++){
-        string x;
-        cin>>x;
         set<string>y;
-        y.insert(x);
+        y.insert(Vname[i]);
         Vset.push_back(y);
     }
 }
-void Makeedge(set<pair<int,pair<string,string>>> &ecost,int edge){
-    for(int i=0;i<edge;i++){
-        string s1,s2;
-        int cost;
-        pair<string,string> edge;
-        cin>>s1>>s2>>cost;
-        int v1=0,v2=0;
-        for(int j=0;j<Vname.size();j++){
-            if(s1==Vname[j])v1=j;
-            if(s2==Vname[j])v2=j;
-            if(v1!=0 && v2!=0)break;
+void read(){
+        int i,j;
+        for(i=0;i<Size;i++){
+        for(j=0;j<Size;j++){
+            if(i==j)graph[i][j]=0;
+            else{
+            graph[i][j]=INT_MAX;
+            
+            }
         }
-        graph[v1][v2]=cost;
-        graph[v2][v1]=cost;
-        edge.first=s1;
-        edge.second=s2;
+    }
+        freopen("kruskal.txt","r",stdin);
+        cin>>edge;
+          for(i=0;i<edge;i++){
+            string x,y;
+            int cost;
+            pair <string,string>edg;
+
+        cin>>x>>y>>cost;
+        if(find(Vname.begin(),Vname.end(),x)==Vname.end())Vname.push_back(x);
+        if(find(Vname.begin(),Vname.end(),y)==Vname.end())Vname.push_back(y);
+        int m,n;
+        m=indexnum(x);
+        n=indexnum(y);
+        graph[m][n]=cost;
+        graph[n][m]=cost;
+        edg.first=x;
+        edg.second=y;
         pair<int,pair<string,string>> edcost;
         edcost.first=cost;
-        edcost.second=edge;
+        edcost.second=edg;
         ecost.insert(edcost);
     }
-}
-int Findset(vector<set<string>> &Vset,string name,int vertex){
+       vertex=Vname.size(); 
+    }
+int Findset(vector<set<string>> &Vset,string name){
     for(int i=0;i<vertex;i++){
-        if(Vset[i].find(name)!= Vset[i].end()){
-            continue;
+        if(Vset[i].find(name) == Vset[i].end()){
+            return -1;
         }
         else return i;
     }
 }
-void kruskal(set<pair<int,pair<string,string>>> &ecost,vector<set<string>> &Vset,int vertex,int edge){
-    Makeset(Vset,vertex);
-    Makeedge(ecost,edge);
+void kruskal(){
     int i,u,v;
     for(auto it :ecost){
-        u=Findset(Vset,it.second.first,vertex);
-        v=Findset(Vset,it.second.second,vertex);
+        u=Findset(Vset,it.second.first);
+        v=Findset(Vset,it.second.second);
         if(u!=v){
-            
+           A.insert(it);
+           //auto it = Vset.begin();
+        //    it=find(Vset.begin(),Vset.end(),Vname[u]);
+        //    (*it).insert(Vname[v]); 
+        Vset[indexnum(it.second.first)].insert(it.second.second);
+           auto itr = Vset.begin()+v;
+           Vset.erase(itr);
         }
     }
+}
+int indexnum(string str){
+    for(int i=0;i<Size;i++){
+        if(Vname[i]==str)return i;
+    }
+    return -1;
+}
+void print(){
+    int minDis=0;
+    for(auto it:A){
+        minDis+=it.first;
+        cout<<it.second.first<<" --> "<<it.second.second<<" : "<<it.first<<endl;
+    }
+    cout<<"Total DIstance : "<<minDis<<endl;
 }
