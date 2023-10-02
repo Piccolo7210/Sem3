ba1d2d814b292959e1cae708d844d81d106f3751
Piccolo7210 <114597646+Piccolo7210@users.noreply.github.com>
2023-10-02 15:50:49 +0600

    Ford added

---
+++ CSE 301/Ford.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define Siz 100
+int ResG[Siz][Siz];
+int graph[Siz][Siz];
+bool bfs( string source,string dest,int parent[]);
+int Ford();
+int indexnum(string str);
+string src;
+string des;
+vector<string> Vname;
+int vertex;
+int edge;
+void read();
+int main(){
+read();
+// for(int i=0;i<vertex;i++){
+//     for(int j=0;j<vertex;j++){
+//         cout<<graph[i][j]<<" ";
+//     }
+//     cout<<endl;
+// }
+// cout<<"Source : "<<src<<endl<<"DEst: "<<des<<endl;
+int maxflow = Ford();
+cout<<"MAX FLOW IS : "<<maxflow<<endl;
+}
+int Ford(){
+    int i,j;
+    int maxFlow=0;
+    for(i=0;i<vertex;i++){
+        for(j=0;j<vertex;j++){
+            ResG[i][j]=graph[i][j];
+        }
+    }
+    int parent[vertex];
+    while(bfs(src,des,parent)){
+        int flow= INT_MAX;
+        int u,v,s;
+        s=indexnum(src);
+        v=indexnum(des);
+        while(v!=s) {
+            u = parent[v];
+            flow = min(flow, ResG[u][v]);
+            v=parent[v];
+        }
+        v=indexnum(des);
+        while(v!=s){
+            u = parent[v];
+            ResG[u][v] -= flow;
+            v=parent[v];
+        }
+        maxFlow += flow;
+    }
+ 
+    return maxFlow;
+    }
+void read(){
+        int i,j,m,n;
+        for(i=0;i<Siz;i++){
+        for(j=0;j<Siz;j++){
+            graph[i][j]=0;
+        }
+    }
+        freopen("ford.txt","r",stdin);
+        cin>>edge;
+          for(i=0;i<edge;i++){
+            string x,y;
+            int cost;
+            cin>>x>>y>>cost;
+            if(find(Vname.begin(),Vname.end(),x)==Vname.end())Vname.push_back(x);
+            if(find(Vname.begin(),Vname.end(),y)==Vname.end())Vname.push_back(y);
+            m=indexnum(x);
+            n=indexnum(y);
+            graph[m][n]=cost;
+    }
+        cin>>src>>des;
+       vertex=Vname.size(); 
+    }
+bool bfs(string source,string dest,int parent[])
+{
+    int s,t;
+    s=indexnum(source);
+    t=indexnum(dest);
+    bool visited[vertex];
+    memset(visited,0,sizeof(visited));
+    queue<int> q;
+    q.push(s);
+    visited[s] = true;
+    parent[s] = -1;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+ 
+        for (int v=0;v<vertex;v++) {
+            if (visited[v] == false && ResG[u][v] > 0) {
+                if (v == t) {
+                    parent[v] = u;
+                    return true;
+                }
+                q.push(v);
+                parent[v] = u;
+                visited[v] = true;
+            }
+        }
+    }
+    return false;
+}
+int indexnum(string str){
+    for(int i=0;i<Siz;i++){
+        if(Vname[i]==str)return i;
+    }
+    return -1;
+}
