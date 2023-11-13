31ca6b7417fe6d93b47f3c76941748960b76c003
Piccolo7210 <114597646+Piccolo7210@users.noreply.github.com>
2023-11-13 17:28:31 +0600

    Update 4.cpp

--- TOC/4.cpp
+++ TOC/4.cpp
@@ -1,203 +1,174 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int num_states, num_symbols, start_state, final_state, state, symbol, num_transition;
-vector<int> states, symbols;
-vector<vector<vector<int>>> transition_table;
-set<set<int>> dfa_states, used_states;
+int state_num,sym_num;
+char starting_state,final_state;
+map<char,int> states_in,symbols_in;
+vector<char> states,symbols;
+vector<vector<vector<char>>> nfa_transition_table;
+set<set<char>> dfa_states,used_states;
 vector<vector<int>> dfa_transition_table;
-vector<int> finals;
 
-void print_dfa_states();
-bool is_visited(set<int> s);
-int get_index(set<int> s);
-bool is_final_state(set<int> s);
+void read_input();
 void print_nfa_transition_table();
+void convert_to_dfa();
+bool is_used(set<char> s);
+void print_dfa_states();
+int get_index_of_set(set<char> st);
 void print_dfa_transition_table();
 
-/*
-    states 0 1 
-    1     12 1
-    12    12 13
-    13    12 1
-*/
-
 int main(){
-    freopen("DFAtoNFA.txt", "r", stdin);
-    //freopen("input2.txt", "r", stdin);
-    cin >> num_states;
-    
-    for(int i = 0; i < num_states; ++i){
-        cin >> state;
-        states.push_back(state);
-    }
-
-    for(int i = 0; i < num_states; ++i) cout << states[i] << " ";
-    cout << endl;
-
-    cin >> num_symbols;
-
-    for(int i = 0; i < num_symbols; ++i){
-        cin >> symbol;
-        symbols.push_back(symbol);
-    }
-
-    for(int i = 0; i < num_symbols; ++i) cout << symbols[i] << " ";
-    cout << endl;
-
-    cin >> start_state >> final_state;
-    cout << "Starting State: " << start_state << endl << "Final State: " << final_state << endl;
-    /*
-        vector<vector<int>> v;
-
-        [                             ]
-        [                             ]
-        [                             ] 
-        v.resize(num_states, vector<int>(num_symbols));
-
-        vector<vector<vector<int>>> transition_table;
-    */
-
-    //transition_table[num_states][num_symbols][num_states];
-    transition_table.resize(num_states, vector<vector<int>>(num_symbols, vector<int> (num_states)));
-
-    for(int i = 0; i < num_states; ++i){
-        for(int j = 0; j < num_symbols; ++j){
-            cin >> num_transition;
-            for(int k = 0; k < num_transition; ++k){
-                cin >> transition_table[i][j][k];
-            }
-        }
-    }
-    
-    print_nfa_transition_table();
-    
-    set<int> st;
-    st.insert(start_state);
-    dfa_states.insert(st); // dfa_states --> {1}
-
-        /*
-        0 {1}
-        1 {1,2}
-        2 {1,3}
-        */
-
-    for(int i = 0; i < dfa_states.size();++i){
-        auto it = next(dfa_states.begin(), i);
-        set<int> ds = *it; //1
-        if(is_visited(ds)) continue; 
-        for(auto sym : symbols){
-            set<int> new_state;
-            for(auto s : ds){ // 1,2 
-                for(auto e : transition_table[s-1][sym]){
-                    if(e != 0) 
-                        new_state.insert(e);
-                }
-                dfa_states.insert(new_state);
-            }
-        }
-        used_states.insert(ds);
-    }
-
+    freopen("lab4.txt","r",stdin);
+    read_input();
+    // print_nfa_transition_table();
+    convert_to_dfa();
     print_dfa_states();
-    //cout << "END" <<endl;
-    
-    dfa_transition_table.resize(dfa_states.size(), vector<int>(num_symbols));
-
-    for(auto ds : dfa_states){ // {1}
-        for(auto sym : symbols){//{0}
-            set<int> new_state;
-            for(auto s: ds){
-                for(auto e : transition_table[s-1][sym]){
-                    if(e != 0) new_state.insert(e);/*union*/                
-                }
-                //                    2   0   ->  ?
-                //dfa_transition_table[s-1][sym] = get_index(new_state);
-            }
-            dfa_transition_table[get_index(ds)][sym] = get_index(new_state);
-        }
-    }
-    /* 0 + 'A'
-        states 0 1 
-        1     idx 1
-        12    12 13
-        13    12 1
-    */
-
-    for(auto s : dfa_states){
-        if(is_final_state(s)) finals.push_back(get_index(s));
-    }
-
     print_dfa_transition_table();
-
-    printf("Starting state: %c\n", *states.begin() - 1 + 'A');  // start_state - 1 + 'A'
-    (finals.size() > 1) ? cout << "Final states: " : cout << "Final state: ";
-    for(auto f : finals){
-        printf("%c ", f + 'A');
+    cout<<endl<<"Starting state of the dfa: 0"<<endl<<endl;
+    cout<<"Final states: ";
+    int i=0;
+    for(auto s:dfa_states){
+        for(auto e:s){
+            if(e==final_state){
+                cout<<i<<" ";
+                break;
+            }
+        }
+        i++;
     }
-    cout << endl;
-    cout << "END" << endl;
-
+    cout<<endl;
     return 0;
 }
 
+void read_input(){
+    cin>>state_num>>sym_num;
+    getchar();
+    char ch;
+    for(int i=0;i<state_num;i++){
+        cin>>ch;
+        states.push_back(ch);
+        states_in[ch]=i;
+        // cout<<states[states_in[ch]]<<" ";
+    }
+    // cout<<endl;
+    getchar();
+    for(int i=0;i<sym_num;i++){
+        cin>>ch;
+        symbols.push_back(ch);
+        symbols_in[ch]=i;
+        // cout<<symbols[symbols_in[ch]]<<" ";
+    }
+    // cout<<endl;
+    getchar();
+    cin>>starting_state>>final_state;
+    // cout<<starting_state<<" "<<final_state<<endl;
+    nfa_transition_table.resize(state_num,vector<vector<char>>(sym_num,vector<char>(state_num)));
+    int transition_num;
+    for(int i=0;i<state_num;i++){
+        for(int j=0;j<sym_num;j++){
+            cin>>transition_num;
+            nfa_transition_table[i][j].clear();
+            for(int k=0;k<transition_num;k++){
+                cin>>ch;
+                nfa_transition_table[i][j].push_back(ch);
+            }
+        }
+    }
+}
+
 void print_nfa_transition_table(){
-    cout << "NFA Transition Table" << endl;
-    for(int i = 0; i < num_states; ++i){
-        cout << "For state: " << states[i] << ": " << endl;
-        for(int j = 0; j < num_symbols; ++j){
-            cout << "For symbol " << symbols[j] << ": ";
-            for(auto s : transition_table[i][j]){
-                if(s > 0) cout << s << " ";
-                else break;
-            }  
-            cout << endl; 
+    for(auto st:states){
+        cout<<"\nFor state-"<<states[states_in[st]]<<":"<<endl;
+        for(auto sym:symbols){
+            cout<<"For symbol-"<<symbols[symbols_in[sym]]<<": ";
+            for(auto e:nfa_transition_table[states_in[st]][symbols_in[sym]]){
+                cout<<e<<" ";
+            }
+            cout<<endl;
         }
-        cout << endl;
     }
 }
 
-void print_dfa_states(){
-    int i = 0;
-    for(auto sets : dfa_states) {
-        printf("State %c == { ", i+'A');
-        for(auto element : sets){
-            cout << element << " ";
+bool is_used(set<char> s){
+    for(auto st:used_states){
+        if(s==st){
+            return true;
         }
-        printf("}\n");
-        i++;
-        cout << endl;
-    }
-}
-
-void print_dfa_transition_table(){
-    cout << "DFA Transition Table" << endl;
-    for(auto s : dfa_transition_table){
-        for(auto e : s)
-            printf("%c ", e + 'A');
-        cout << endl;
-    }
-}
-
-bool is_visited(set<int> s){
-    for(auto element : used_states){
-        if(s == element) return true;   
     }
     return false;
 }
 
-int get_index(set<int> s){
-    int i = 0;
-    for(auto sets : dfa_states){
-        if(sets == s) return i;
+void convert_to_dfa(){
+    dfa_states.insert({starting_state});
+    for(int i=0;i<dfa_states.size();i++){
+        auto it=next(dfa_states.begin(),i);
+        set<char> ds=*it;
+        if(is_used(ds)){
+            continue;
+        }
+        for(auto sym:symbols){
+            set<char> st;
+            for(auto s:ds){
+                for(auto e:nfa_transition_table[states_in[s]][symbols_in[sym]]){
+                    st.insert(e);
+                }
+            }
+            dfa_states.insert(st);
+        }
+        used_states.insert(ds);
+    }
+    dfa_transition_table.resize(dfa_states.size(),vector<int>(sym_num));
+    for(auto ds:dfa_states){
+        for(auto sym:symbols){
+            set<char> st;
+            for(auto s:ds){
+                for(auto e:nfa_transition_table[states_in[s]][symbols_in[sym]]){
+                    st.insert(e);
+                }
+            }
+            dfa_transition_table[get_index_of_set(ds)][symbols_in[sym]]=get_index_of_set(st);
+        }
+    }
+}
+
+void print_dfa_states(){
+    int i=0;
+    cout<<endl;
+    cout<<"States for the DFA:"<<endl;
+    for(auto s:dfa_states){
+        cout<<i++<<"={";
+        for(auto e:s){
+            cout<<" "<<e;
+        }
+        cout<<" }"<<endl;
+    }
+    cout<<endl;
+}
+
+int get_index_of_set(set<char> st){
+    int i=0;
+    for(auto s:dfa_states){
+        if(s==st){
+            return i;
+        }
         i++;
     }
     return -1;
 }
 
-bool is_final_state(set<int> s){
-    for(auto e : s){
-        if(e == final_state) return true;
+void print_dfa_transition_table(){
+    cout<<"Transition table for the DFA:"<<endl;
+    cout<<"\t";
+    for(auto sym:symbols){
+        cout<<"Sym-"<<sym<<"\t";
     }
-    return false;
+    cout<<endl;
+    int i=0;
+    for(auto s:dfa_states){
+        cout<<"State-"<<i++<<"\t";
+        for(auto sym:symbols){
+            cout<<dfa_transition_table[get_index_of_set(s)][symbols_in[sym]]<<"\t";
+        }
+        cout<<endl;
+    }
 }
