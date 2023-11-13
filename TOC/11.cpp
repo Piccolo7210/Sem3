83c428e7b907fc007306785ea9c5ea16d2247b1d
Piccolo7210 <114597646+Piccolo7210@users.noreply.github.com>
2023-11-13 17:21:20 +0600

    Update 11.cpp

--- TOC/11.cpp
+++ TOC/11.cpp
@@ -1,108 +1,131 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int found = 0,found2=0;
-string path;
-vector<string>paths,paths2;
+string str;
+vector<string> productions={"S","aS","aSbS",""};
+vector<string> leftmost_output1,leftmost_output2;
 
-void cfg(vector<string> &base, string output, string target, string currpath)
-{
-    if (found>=2)
-        return;
+void print_derivation(vector<string> derivation);
 
-    if (output.size()>target.size()+2)
-        return;
 
-    if (output.find('S')==string::npos && output!=" ")
-    {
-        if (output==target)
-        {
-            found++;
-            path = currpath;
-            paths.push_back(path);
+int main(){
+    freopen("lab11.txt","r",stdin);
+    cin>>str;
+    if(str[0]=='b'){
+        cout<<str<<" cannot be derived using this grammar"<<endl;
+        cout<<"The grammar may not be ambiguous."<<endl;
+        return 0;
+    }
+    if(str[0]=='a'&&str[1]=='b'){
+        cout<<str<<" cannot be derived using production 'aS'"<<endl;
+        cout<<"The grammar may not be ambiguous."<<endl;
+        return 0;
+    }
+    int idx,index;
+    leftmost_output1.push_back("S");
+    string temp,sub1,sub2,sub3;
+    int i,cur=0;
+    for(i=0;i<str.size();i++){
+        temp=leftmost_output1.back();
+        if(str[i]=='a'){
+            if(i+1!=str.size()&&str[i+1]=='b'){
+                i++;
+                sub3="aSbS";
+            }
+            else{
+                sub3="aS";
+            }
+            sub1=temp.substr(0,temp.size()-1);
+            temp=sub1+sub3;
+            leftmost_output1.push_back(temp);
         }
-        return;
+    }
+    temp=leftmost_output1.back();
+    while((idx=temp.find('S'))!=string::npos){
+        sub1=temp.substr(0,idx);
+        sub2=temp.substr(idx+1);
+        temp=sub1+sub2;
+        leftmost_output1.push_back(temp);
+    }
+    temp=leftmost_output1.back();
+    if(temp!=str){
+        cout<<str<<" is not in the grammar!"<<endl;
+        return 0;
+    }
+    print_derivation(leftmost_output1);
+    leftmost_output2.push_back("S");
+    leftmost_output2.push_back("aSbS");
+    idx=str.find('b');
+    if(idx==string::npos){
+        cout<<str<<" cannot be derived using production 'aSbS'"<<endl;
+        cout<<"The grammar may not be ambiguous."<<endl;
+        return 0;
     }
 
-    for (string x : base)
-    {
-        if (output.find('S')!=string::npos)
-        {
-            string temp = output.substr(0, output.find('S'));
-            //cout<<"hello"<<endl;
-            temp += x;
-            temp += output.substr(output.find('S') + 1);
-            x = temp;
-        }
-       // return;
-        cfg(base, x, target, currpath+"->"+x);
-        if (found>=2)
-            return;
+    for(i=1;i<idx;i++){
+        temp=leftmost_output2.back();
+        index=temp.find('S');
+        sub1=temp.substr(0,index);
+        sub2=temp.substr(index);
+        temp=sub1+"a"+sub2;
+        leftmost_output2.push_back(temp);
     }
-}
-
-void cfg2(vector<string> &base, string output, string target, string currpath)
-{
-    if (found2>=2)
-        return;
-
-    if (output.size()>target.size()+2)
-        return;
-
-    if (output.find('S')==string::npos && output!=" ")
-    {
-        if (output==target)
-        {
-            found2++;
-            path = currpath;
-            paths2.push_back(path);
+    temp=leftmost_output2.back();
+    index=temp.find('S');
+    sub1=temp.substr(0,index);
+    sub2=temp.substr(index+1);
+    temp=sub1+sub2;
+    leftmost_output2.push_back(temp);
+    for(i++;i<str.size();i++){
+        temp=leftmost_output2.back();
+        sub1=temp.substr(0,temp.size()-1);
+        if(i+1<str.size()&&str[i+1]=='b'){
+            sub2="aSbS";
         }
-        return;
-    }
-
-    for (string x : base)
-    {
-        if (output.find('S')!=string::npos)
-        {
-            string temp = output.substr(0, output.find_last_of('S'));
-            temp += x;
-            temp += output.substr(output.find_last_of('S') + 1);
-            x = temp;
+        else{
+            sub2="aS";
         }
-        cfg2(base, x, target, currpath+"->"+x);
-        if (found2>=2)
-            return;
+        temp=sub1+sub2;
+        leftmost_output2.push_back(temp);
+        if(i+1<str.size()&&str[i+1]=='b'){
+            index=temp.find('S');
+            sub1=temp.substr(0,index);
+            sub2=temp.substr(index+1);
+            temp=sub1+sub2;
+            leftmost_output2.push_back(temp);
+            i++;
+        }
     }
-}
-
-int main()
-{
-    vector<string> base = {"aS", "aSbS", ""};
-    string s;
-    cin >> s;
-    cfg(base, " ", s, path);
-
-    // if(found==2)
-    // {
-    //     for(string x : paths)
-    //     cout<<"Letfmost : S"+x<<endl;
-    //     cout<<"The grammar is ambiguous"<<endl;
-    // }
-    // else
-    // cout<<"May not be ambiguous"<<endl;
-
-    //RightMost derivation test for ambiguous grammar
-    // path.clear();
-
-    // cfg2(base, " ", s, path);
-
-    // if(found2==2)
-    // {
-    //     for(string x : paths2)
-    //     cout<<"Rightmost: S"+x<<endl;
-    //     cout<<"The grammar is ambiguous"<<endl;
-    // }
-    // else
-    // cout<<"May not be ambiguous"<<endl;
+    temp=leftmost_output2.back();
+    while((idx=temp.find('S'))!=string::npos){
+        sub1=temp.substr(0,idx);
+        sub2=temp.substr(idx+1);
+        temp=sub1+sub2;
+        leftmost_output2.push_back(temp);
+    }
+    temp=leftmost_output2.back();
+    if(temp!=str){
+        cout<<str<<" is not in the grammar!"<<endl;
+        cout<<"The grammar may not be ambiguous."<<endl;
+        return 0;
+    }
+    print_derivation(leftmost_output2);
+    cout<<"The grammar is ambiguous."<<endl;
     return 0;
+}
+
+
+
+void print_derivation(vector<string> derivation){
+    if(derivation[1]=="aSbS"){
+        cout<<"Leftmost 2: ";
+    }
+    else{
+        cout<<"Leftmost 1: ";
+    }
+    cout<<derivation[0];
+    for(int i=1;i<derivation.size();i++){
+        cout<<" => "<<derivation[i];
+    }
+    cout<<endl;
 }
